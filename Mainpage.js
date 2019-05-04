a34a45accd1910ad2a74f0e07192bc181c0838be
Cristian Ortega Singer <cris.ortega@fau.de>
2019-05-04 22:21:37 +0200

    Added display-event for Cards

--- Mainpage.js
+++ Mainpage.js
@@ -24,3 +24,20 @@ function displayModalC(imgId){
   modal.style.display = "block";
   modalImg.src = image.src;
 }
+
+function displayCard(cardId){
+  var card = document.getElementById(cardId);
+  card.style.display = "inline";
+  displayModal("modal3",3);
+  alert("Du hast eine Karte aufgesammelt!");
+}
+function displayAllCards(){
+  var cards = ["e1","e2","e3","e4","f1","f2","f3","f4"];
+  cards.forEach(allCards);
+  displayModal("modal3",3);
+  alert("All cards displayed");
+}
+function allCards(value){
+  var card = document.getElementById(value);
+  card.style.display = "inline";
+}
