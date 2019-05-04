ddbdcef29293212f09ffbca86e13a03c2bfca3eb
Cristian Ortega Singer <cris.ortega@fau.de>
2019-05-04 21:07:18 +0200

    Uploaded CardImages, Placed in CardAlbumModal

--- Mainpage.js
+++ Mainpage.js
@@ -13,3 +13,14 @@ function displayModal(modalId,closeIndex){
     modal.style.display = "none";
   }**/
 }
+
+function displayModalC(imgId){
+  var modal = document.getElementbyId("modal4");
+  var modalImg = document.getElementById("card");
+  var backgrModal= document.getElementById("modal3");
+  var image = document.getElementById(imgId);
+  backgrModal.blur();
+  modal.focus();
+  modal.style.display = "block";
+  modalImg.src = image.src;
+}
