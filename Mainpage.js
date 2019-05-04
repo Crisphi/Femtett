2136446b1b05fdc785cb0889bd57546ad55916f7
Cristian Ortega Singer <cris.ortega@fau.de>
2019-05-05 00:34:58 +0200

    added cardCounter for keeping track how many cards are collected added overlay for ending (placeholder)

--- Mainpage.js
+++ Mainpage.js
@@ -1,4 +1,4 @@
-
+var cardCounter = 0;
 
 function displayModal(modalId,closeIndex=0){
   var modal = document.getElementById(modalId);
@@ -8,33 +8,36 @@ function displayModal(modalId,closeIndex=0){
   span.onclick = function(){
     modal.style.display = "none";
   }
-
-  /**window.onclick = function(event){
-    modal.style.display = "none";
-  }**/
 }
 
 function overlayOn(imgId){
   var overlayImg = document.getElementById("card");
   var image = document.getElementById(imgId);
   overlayImg.src = image.src;
-  displayModal("overlay");
+  displayModal("cardOverlay");
 }
-function overlayOff(){
-  document.getElementById("overlay").style.display = "none";
+function overlayOff(ovId){
+  document.getElementById(ovId).style.display = "none";
 }
 
 function displayCard(cardId){
   var card = document.getElementById(cardId);
   card.style.display = "inline";
+  displayModal("modal4",4);
   displayModal("modal3",3);
-  alert("Du hast eine Karte aufgesammelt!");
+  cardCounter++;
+  if(cardCounter == 8){
+    displayModal("ending");
+  }
 }
+
+/** just for testing**/
 function displayAllCards(){
   var cards = ["e1","e2","e3","e4","f1","f2","f3","f4"];
   cards.forEach(allCards);
   displayModal("modal3",3);
   alert("All cards displayed");
+  displayModal("ending",5);
 }
 function allCards(value){
   var card = document.getElementById(value);
