674841f12dabcdd37ca40eb87c7b9ce834c90f9f
Cristian Ortega Singer <cris.ortega@fau.de>
2019-05-16 00:36:48 +0200

    added function display zoom in lightbox

--- Mainpage.js
+++ Mainpage.js
@@ -11,6 +11,10 @@ var cardAreas = {
   "f3":"574,291,622,284,672,291,672,444,574,444",
   "f4":"732,291,778,284,831,291,831,444,732,444"
 };
+var lightAreas = {
+  "lightArea1":"78,330,581,422",
+  "lightArea2":"109,174,552,386"
+}
 var zoomSrc = {
   "b1Clip":{"margin-left": "-100px", "margin-top": "-320px", "transform": "scale(0.9,0.9)"},
   "b2Clip":{"margin-left": "-349px", "margin-top": "-750px", "transform": "scale(1.1,1.1)"},
@@ -22,6 +26,8 @@ var zoomSrc = {
   "com4Clip":{"margin-left": "-1000px", "margin-top": "-650px", "transform": "scale(1,1)"},
   "noteClip":{"margin-left": "-800px", "margin-top": "-650px", "transform": "scale(1,1)"},
 
+  "one":{"margin-left": "0px", "margin-top": "-320px", "transform": "scale(1.3,1.3)"},
+  "two":{"margin-left": "-100px", "margin-top": "-320px", "transform": "scale(0.9,0.9)"}
 };
 var slideIndex = 0;
 slides=["Sources/Marburger_Profs_Notiz.jpg","Sources/Marburger_Profs_Folie.jpg","Sources/Marburger_Profs_Foto.jpg"];
@@ -74,6 +80,21 @@ function createArea(areaId,cardId){
   area.href = "javascript:overlayOn('"+ cardId+ "','card','cardOverlay');";
 }
 
+function createLightArea(areaId,imgId, slide){
+  var coords =  lightAreas[areaId];
+  var area = document.getElementById("lightArea");
+  area.coords = coords;
+  area.href = "javascript:displayLightZoom('"+ imgId+ "','zoomed','zoomOverlay','"+slide+"');";
+}
+
+function displayLightZoom(imgId, ovImg, overlay, slide){
+  var zoomed = document.getElementById("zoomed");
+  zoomed.style.marginLeft = zoomSrc[slide]["margin-left"];
+  zoomed.style.marginTop = zoomSrc[slide]["margin-top"];
+  zoomed.style.transform = zoomSrc[slide]["transform"];
+  overlayOn(imgId, ovImg, overlay);
+}
+
 function displayLightroom(){
   var slideImg = document.getElementById("slideImg");
   slideIndex = 0;
@@ -96,12 +117,16 @@ function updateImage(){
         //set up the new image
         new_image.id = "slideImg";
         new_image.src = slides[slideIndex];
-        new_image.usemap = "#lightMap";
+        new_image.useMap = "#lightMap";
 
         if(slideIndex == 1){
-          createArea("lightArea1","slideImg","zoomed","zoomOverlay");
+          createLightArea("lightArea1","slideImg","one");
         }else if (slideIndex == 2) {
-          createArea("lightArea2","slideImg","zoomed","zoomOverlay");
+          createLightArea("lightArea2","slideImg","two");
+        }else{
+          var area = document.getElementById("lightArea");
+          area.coords = "";
+          area.href ="";
         }
         // insert new image and remove old
         image.parentNode.insertBefore(new_image,image);
